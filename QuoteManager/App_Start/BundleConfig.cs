545ab75901e5f07bce113de17cbf49ce3e738db0
cjones <christopher.jones@sita.aero>
2016-02-29 21:28:54 +0100

    Kendo installation

--- QuoteManager/App_Start/BundleConfig.cs
+++ QuoteManager/App_Start/BundleConfig.cs
@@ -14,6 +14,10 @@ namespace QuoteManager
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                        "~/Scripts/kendo/kendo.all.min.js",
+                        "~/Scripts/kendo/kendo.aspnetmvc.min.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -23,9 +27,15 @@ namespace QuoteManager
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new StyleBundle("~/Content/kendo/css").Include(
+                    "~/Content/kendo/kendo.common-bootstrap.min.css",
+                    "~/Content/kendo/kendo.bootstrap.min.css"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.IgnoreList.Clear();
         }
     }
 }
